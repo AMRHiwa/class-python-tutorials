3677d034e55d262b0b69bb8cbe3428718411ce15
Hiwa Azizi <74157596+AMRHiwa@users.noreply.github.com>
2022-02-09 16:04:42 +0330

    update matplotlib practice 2

--- matplotlib.pranctice2.py
+++ matplotlib.pranctice2.py
@@ -18,7 +18,7 @@ plt.plot([5, 6, 11], "purple")      #make the chart
 plt.show()       #show the chart
 
 ---------------------------------------------------------
-"""
+
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -32,3 +32,34 @@ plt.ylabel("f(x) = X^2")  #create a label for y
 plt.title("the chart!")   #create a title for chart
 
 plt.show()                #show a chart
+---------------------------------------------------------
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+x1 = ["Farvardin", "Ordibehesht", "Khordad"]
+x2 = [10, 20, 30]                           # must len(x1) = len(x2)
+
+y1 = ["Math", "Chemestri", "Programming"]
+y2 = [40, 50, 20]                           # must len(y1) = len(y2)
+
+plt.plot(x1, y1)
+
+plt.xticks(x2, x1)                          # We attribute x1 to x2
+plt.yticks(y2, y1)                          # We attribute y1 to y2
+
+plt.show()
+---------------------------------------------------------
+"""
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.arange(0, 10, 0.01) # make a array for time (sec)
+y = np.sin(2*np.pi*t)   # use sin function from numpy
+
+plt.plot(t, y)         #  create a chart for sin
+plt.xlabel("time")     # label for x
+plt.ylabel("output")   # label for y
+plt.title("Sin")       # title for this chart
+plt.show()             # show the chart
