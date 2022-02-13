78dda3fb80fe7debfe4b8a8ed3afe0275a3f91ad
Hiwa Azizi <azizi.mr1377@gmail.com>
2022-02-13 16:14:15 +0330

    update datetime practice

--- datetime_practice_file.py
+++ datetime_practice_file.py
@@ -1,6 +1,4 @@
 
-
-"""
 #-------example 1----------
 from dataclasses import dataclass
 import datetime
@@ -49,5 +47,67 @@ timestamp = date.fromtimestamp(2525252525)
 
 # show the info
 print(timestamp)
-"""
+
 #--------example 6------------
+from datetime import date
+
+# create a variable to storage a date
+today = date.today()
+
+# show the day
+print(f"day of today: {today.day}")
+
+# show the month
+print(f"month of today: {today.month}")
+
+# show the year
+print(f"year of today: {today.year}")
+
+#--------example 7-----------
+from datetime import time
+
+# time(hour = 0, minute = 0, second = 0)
+a = time()
+print("a = ", a)
+
+# time (hour and minute and second)
+b = time(11, 34, 56)
+print("b = ", b)
+
+# time(hour , minute , second)
+c = time(hour= 11, minute= 34, second= 56)
+print("c = ", c)
+
+# time(hour, minute, second, microsecond)
+d = time(11, 34, 56, 234566)
+print("d = ",d)
+
+#----------example 8---------
+from datetime import time
+
+# make a variable to storage a time
+time1 = time(11, 34, 56)
+
+# show the second
+print(f"second : {time1.second}")
+
+# show the minute
+print(f"minute : {time1.minute}")
+
+# show the hour 
+print(f"hour : {time1.hour}")
+
+#print(dir(time))
+
+#----------example 9----------
+from datetime import datetime
+
+# datetime(year, month, day)
+a = datetime(1997, 2, 18)
+print("date of burn of love \na = ", a)
+
+# datetime(year, month, day, hour, minute, second)
+b = datetime(1997,2,18,13,30,40)
+print("b = ", b)
+
+#--------example 10-----------
