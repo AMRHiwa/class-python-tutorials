9bd23623ece49c6cd840f4b0b22031ea591e031c
Hiwa Azizi <azizi.mr1377@gmail.com>
2022-02-16 15:08:39 +0330

    create a time practice file

---
+++ time_practice.py
@@ -0,0 +1,13 @@
+"""
+author : Hiwa Azizi
+time modul in python and it's methods
+2022
+"""
+# --------example 1----------
+import time
+
+# create a variable to storage the value of seconds past of epoch
+seconds = time.time()
+
+# show the result
+print(f"seconds since epoch : {seconds}")
