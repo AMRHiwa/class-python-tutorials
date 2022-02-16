e938d0329db35e598607fbe7ddff2b21a17d27e9
Hiwa Azizi <azizi.mr1377@gmail.com>
2022-02-16 15:50:34 +0330

    update the time practice

--- time_practice.py
+++ time_practice.py
@@ -11,3 +11,38 @@ seconds = time.time()
 
 # show the result
 print(f"seconds since epoch : {seconds}")
+
+
+
+
+
+# --------example 2---------
+import time
+
+# create a variable to storage the value of seconds past of epoch
+seconds = time.time()
+
+# create a variable to storage the calculate of local time with epoch time
+local_time = time.ctime(seconds)
+
+# show the result
+print(f"seconds since epoch : {local_time}")
+
+
+
+
+
+# -------example 3----------
+import time
+
+print("this is a printed immediately")
+
+# Create an interrupt with time.sleep
+time.sleep(2)
+
+print("this is a printed after 2 seconds")
+
+
+
+
+
