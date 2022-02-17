7dbd1050b0c3e27f0977cb9eba03bfb5f93cfd47
AMR-Hiwa <azizi.mr1377@gmail.com>
2022-02-17 12:11:34 +0330

    add the math module practice file

---
+++ math_module_practice.py
@@ -0,0 +1,36 @@
+"""
+this file wrote by AMR Hiwa 
+for practicing with math module and it's methods
+2022
+
+"""
+
+# -----example 1------
+
+# first of all we have to import math module with import order or from ... import
+import math 
+
+# floor method 
+# we show the ability of floor function from math with a different values
+print("num is 13.25315 ")
+print("num1 with floor method : ", math.floor(13.25315))
+
+print("num2 is 25")
+print(f"num2 with floor method : {math.floor(25)}")
+
+print("num3 is -12.6")
+print("num3 with floor method : {}".format(math.floor(-12.6)))
+
+
+
+# ceil method
+# we show it's ability with different values
+print("num is 13.25315 ")
+print("num1 with ceil method : ", math.ceil(13.25315))
+
+print("num2 is 25")
+print(f"num2 with ceil method : {math.ceil(25)}")
+
+print("num3 is -12.6")
+print("num3 with ceil method : {}".format(math.ceil(-12.6)))
+
