0e6e076142586e230200a8fb406c5d6d644b76f4
Hiwa Azizi <azizi.mr1377@gmail.com>
2022-09-19 01:20:42 +0430

    update Encryption

--- Encrypt and Decrypt With Python.py
+++ Encrypt and Decrypt With Python.py
@@ -11,22 +11,36 @@ def generatekey():
     #           that 'b' in this statement means bit and that string in '...' is our key
     # if we want to show that key in string, we can use this command 
     #       print(str(key, 'utf-8'))
-    print(key)
-    print(str(key, 'utf-8'))
+    
+    # print(key)
+    # print(str(key, 'utf-8'))
     return key
 
 # now we need a function to Encrypt the message or text
 def encryption(themessage, key):
     # we need a value to store the generated key that we do it in this way
-    #       the_value_name = Fernet(value of key in byte type) like
+    #       the_value_name = Fernet(value of key in byte type) 
+    #   like :
     #       key1 = Fernet(b'_9uPhouCcO5bBm6o68ULD-mt6AV1qjKnmxUNH8Vl2sk=')
     storekey = Fernet(key)
-
+    
     # now we need to encrypt the text or message that we can give it straight or
     # import that from another text's file and also we need a value to save encrypt message
     # we do it by this method from Fernet
-    #       value's_name = the_value_of_store_key.encrypt(message in byte type) like
+    #       value's_name = the_value_of_store_key.encrypt(message in byte type) 
+    #   like :
     #       encryptmessage = storekey.encript(b'This is tutorial encrypting by python')
-    #       or we can give it address a text file to import a text
+    #       or we can give it text from outside the function by argument
+    encryptmessage = storekey.encrypt(themessage)
+    print(f"the encrypt message is : ",str(encryptmessage, 'utf-8'))
+    return encryptmessage
 
-generatekey()
+def decryption(encryptmessage, key):
+    storekey = Fernet(key)
+    decryptionmessage = storekey.decrypt(encryptmessage)
+    print(str(decryptionmessage, "utf-8"))
+
+key = generatekey()
+the_encrypt_message = encryption(b'hello', key)
+print(f'the encrypt message in body program : ', str(the_encrypt_message, "utf-8"))
+decryption(the_encrypt_message, key)
