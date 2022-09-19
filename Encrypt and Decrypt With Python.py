6423f45f444f475451c43bf3e634fb6ae2bad59a
Hiwa Azizi <azizi.mr1377@gmail.com>
2022-09-19 18:03:04 +0430

    last update for ecryption and decryption text by python

--- Encrypt and Decrypt With Python.py
+++ Encrypt and Decrypt With Python.py
@@ -3,9 +3,11 @@ from cryptography.fernet import Fernet
 
 # we need a function to generate a key
 def generatekey():
+
     # for genarating a key to encrypt and decrypt files we use this method from Fernet module
     #     values_name = Fernet.generate_key()
     key = Fernet.generate_key()
+    
     # the generated key with this function is bit and if we want to show that
     #           we have some thing like this:
     #           b'_9uPhouCcO5bBm6o68ULD-mt6AV1qjKnmxUNH8Vl2sk='
@@ -15,10 +17,13 @@ def generatekey():
     
     # print(key)
     print(f'key is : ', str(key, 'utf-8'))
+    
+    # we can also return the key into the program's main body
     return key
 
 # now we need a function to Encrypt the message or text
 def encryption(themessage, key):
+    
     # we need a value to store the generated key that we do it in this way
     #       the_value_name = Fernet(value of key in byte type) 
     #   like :
@@ -33,11 +38,17 @@ def encryption(themessage, key):
     #       encryptmessage = storekey.encrypt(b'This is tutorial encrypting by python')
     #       or we can give it text from outside the function by argument
     encryptmessage = storekey.encrypt(themessage)
+    
+    # we can show the encrypted message to user 
     print(f"the encrypt message is : ",str(encryptmessage, 'utf-8'))
+    
+    # in this program another ability we can add it that is return the encrypted message
+    # into main body if we want it by the 'return' command
     return encryptmessage
 
 # we need a function to decryption of encrypt message
 def decryption(encryptmessage, key):
+    
     # we need a value to store the generated key that we do it in this way
     #       the_value_name = Fernet(value of key in byte type) 
     #   like :
@@ -52,9 +63,20 @@ def decryption(encryptmessage, key):
     #       decryptionmessage = storekey.decrypt(b'This is tutorial encrypting by python')
     #       or we can give it text from outside the function by argument
     decryptionmessage = storekey.decrypt(encryptmessage)
+    
+    # in this function we can also show the decrypted message to our user
     print(f'the decrypt message : ',str(decryptionmessage, "utf-8"))
 
+# for a build a key we have to call the generatekey function that we defined above
 key = generatekey()
+
+# for encrypting the message we must call the encryption function that defined above before
+# and give it two arguments , a message with byte type and the key that used for encrypting
+# for set a message in a byte type we can set it in " b'the message text' " format
 the_encrypt_message = encryption(b'hello', key)
+
 # print(f'the encrypt message in body program : ', str(the_encrypt_message, "utf-8"))
+
+# for decrypt the encrypted message we must call the decryption function that defined above before
+# and give it two arguments, a encrypted message in byte type and the key that used for encrypt
 decryption(the_encrypt_message, key)
